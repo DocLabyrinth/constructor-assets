3946536b0b484698a169084f5531819debbf3a35
b <a@b.com>
2015-05-06 12:29:49 +0200

    re-enabled sprite extraction with ruby

--- extract.go
+++ extract.go
@@ -7,11 +7,13 @@ import (
   "os"
   "strings"
   "io/ioutil"
-  "log"
   "path/filepath"
+  "image"
+  "image/png"
+  "image/color"
 )
 
-type IndexItem struct {
+type FilIndexItem struct {
   filename string
   offset uint32
   length uint32
@@ -19,7 +21,25 @@ type IndexItem struct {
   end uint32
 }
 
-func getIffIndex(sub_dir string, fil_file *os.File) ([]IndexItem) {
+type LevelIndexItem struct {
+  id string
+  offset uint32
+  length uint32
+}
+
+type ColorMapEntry struct {
+  transparent bool
+  r uint8
+  g uint8
+  b uint8
+}
+
+type SpriteItem struct {
+  offset uint32
+  length uint32
+}
+
+func GetIffIndex(sub_dir string, fil_file *os.File) ([]FilIndexItem) {
   // get the encrypted value for the number of entries in the file
   var (
     num_entries uint32
@@ -29,16 +49,16 @@ func getIffIndex(sub_dir string, fil_file *os.File) ([]IndexItem) {
   buffer := bufio.NewReader(fil_file)
   enc_entries := make([]byte, 4)
 
-  file_info, err := file.Stat()
+  file_info, err := fil_file.Stat()
   if err != nil {
-    fmt.Println("Fatal: ", err)
-    log.Fatal(err)
+    fmt.Println("Failed to stat DATA.FIL file: ", err)
+    panic(err)
   }
 
   _, err = buffer.Read(enc_entries)
   if err != nil {
-    fmt.Println("Fatal: ", err)
-    log.Fatal(err)
+    fmt.Println("Failed to read the number of entries", err)
+    panic(err)
   }
 
   // decrypt the value to get the number of entries
@@ -46,12 +66,12 @@ func getIffIndex(sub_dir string, fil_file *os.File) ([]IndexItem) {
   fmt.Println("entries: ", num_entries)
 
   index_buf := make([]byte, 17 * num_entries)
-  index := make([]IndexItem, num_entries)
+  index := make([]FilIndexItem, num_entries)
 
   _, err = buffer.Read(index_buf)
   if err != nil {
-    fmt.Println("Fatal: ", err)
-    log.Fatal(err)
+    fmt.Println("Failed to read the index data", err)
+    panic(err)
   }
 
   // decrypt the index
@@ -95,7 +115,7 @@ func getIffIndex(sub_dir string, fil_file *os.File) ([]IndexItem) {
   return index
 }
 
-func extractFile(sub_dir string) {
+func ExtractFile(sub_dir string) {
   var (
     file *os.File
     err error
@@ -107,13 +127,12 @@ func extractFile(sub_dir string) {
   file, err = os.Open(file_path)
   if err != nil {
     fmt.Println("Fatal: ", err)
-    log.Fatal(err)
+    panic(err)
   }
-
   defer file.Close()
 
   fmt.Println("*****", sub_dir)
-  index := getIffIndex(sub_dir, file)
+  index := GetIffIndex(sub_dir, file)
   fmt.Println("The index:\n", index, "\n")
   for _, ind_item := range index {
     fmt.Println("filename:", ind_item.filename, "\n", "offset", ind_item.offset, "length", ind_item.length, "i_start", ind_item.start,"i_end",ind_item.end)
@@ -138,7 +157,7 @@ func extractFile(sub_dir string) {
   file_info, err := file.Stat()
   if err != nil {
     fmt.Println("Failed to get info about the archive file", err)
-    log.Fatal(err)
+    panic(err)
   }
 
   for entry_i, item := range index {
@@ -167,7 +186,6 @@ func extractFile(sub_dir string) {
       fmt.Println("Failed to create ", out_path)
       panic(err)
     }
-
     defer out_file.Close()
 
 
@@ -186,17 +204,361 @@ func extractFile(sub_dir string) {
     _, err = out_file.Write(entry)
     if err != nil {
       fmt.Println("Failed to write file ", out_path, " for entry ", entry_i)
-      log.Fatal(err)
+      panic(err)
     }
 
     fmt.Println("Extracted ", out_path)
   }
 }
 
+func GetLevelIndex(level_file *os.File) (map[string]*LevelIndexItem) {
+  var err error
+
+  /*
+  The whole file is in IFF format, wrapped in a FORM chunk with a MAPS sub-header.
+  This data is unimportant for further parsing, so skip past it.
+  */
+  _, err = level_file.Seek(12, 0)
+  if err != nil {
+    fmt.Println("Failed to seek past header in level file")
+    panic(err)
+  }
+
+  index := make(map[string]*LevelIndexItem)
+  position := 12
+
+  for level_i := 0; level_i < 6; level_i++ {
+    chunk_id := make([]byte, 4)
+    chunk_length := make([]byte, 4)
+    index_item := new(LevelIndexItem)
+
+    _, err = level_file.Read(chunk_id)
+    if err != nil {
+      fmt.Println("Failed to read chunk id", level_i, "in level file")
+      panic(err)
+    }
+    index_item.id = string(chunk_id)
+
+    _, err = level_file.Read(chunk_length)
+    if err != nil {
+      fmt.Println("Failed to read data length for chunk ", level_i, "in level file")
+      panic(err)
+    }
+    index_item.length = binary.BigEndian.Uint32(chunk_length)
+
+    position += 8
+    index_item.offset = uint32(position)
+
+    _, err = level_file.Seek(int64(index_item.length), 1)
+    if err != nil {
+      fmt.Println("Failed to seek past chunk ", level_i, "in level file")
+      panic(err)
+    }
+
+    index[index_item.id] = index_item
+
+    // 4 characters + uin32 + chunk length
+    position += (8 + int(index_item.length))
+  }
+
+  return index
+}
+
+func GetLevelColorMap(level_file *os.File) ([]ColorMapEntry) {
+  var err error
+
+  level_index := GetLevelIndex(level_file)
+
+  color_map := make([]ColorMapEntry, 256)
+  _, err = level_file.Seek(int64(level_index["CMAP"].offset), 0)
+  if err != nil {
+    fmt.Println("Failed to open level file")
+    panic(err)
+  }
+
+  colors := make([]byte, 3)
+  for color_i := range(color_map) {
+    _, err:= level_file.Read(colors)
+    if err != nil {
+      fmt.Println("Failed to open level file")
+      panic(err)
+    }
+    color_map[color_i].r = colors[0]
+    color_map[color_i].g = colors[1]
+    color_map[color_i].b = colors[2]
+  }
+
+  return color_map
+}
+
+func ExtractSprite(sprite_file *os.File, out_path string, info SpriteItem, cmap []ColorMapEntry) {
+  //fmt.Println("trying to draw this sprite", info)
+
+  var err error
+  /*
+  There are two uint16's at the start of the sprite
+  header whose value is unknown, so seek past them
+  */
+  _, err = sprite_file.Seek(int64(info.offset) + 4, 0)
+  if err != nil {
+    fmt.Println("Failed to seek to offset", info.offset, "in sprite file")
+    panic(err)
+  }
+
+  width_buf := make([]byte, 2)
+  height_buf := make([]byte, 2)
+
+  _, err = sprite_file.Read(width_buf)
+  if err != nil {
+    fmt.Println("Failed to read sprite width")
+    panic(err)
+  }
+
+  _, err = sprite_file.Read(height_buf)
+  if err != nil {
+    fmt.Println("Failed to read sprite height")
+    panic(err)
+  }
+
+  out_file, err := os.OpenFile(out_path, os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0644)
+  if err != nil {
+    fmt.Println("Failed to create output image", out_path)
+    panic(err)
+  }
+
+  //width := int(binary.BigEndian.Uint16(width_buf))
+  //height := int(binary.BigEndian.Uint16(height_buf))
+  width := int(binary.LittleEndian.Uint16(width_buf))
+  height := int(binary.LittleEndian.Uint16(height_buf))
+  fmt.Println("Image is ", width, "x", height)
+
+  imgRect := image.Rect(0, 0, width, height)
+  img := image.NewNRGBA(imgRect)
+
+  var last_was_zero bool
+
+  pixels := make([]ColorMapEntry, width * height)
+  pixel_buf := make([]byte, info.length)
+
+  _, err = sprite_file.Read(pixel_buf)
+  if err != nil {
+    fmt.Println("Failed to read sprite height")
+    panic(err)
+  }
+
+  // lookup the color values and decompress transparency
+  var trans_entry ColorMapEntry
+  trans_entry.transparent = true
+
+  for pixel_i := range pixel_buf {
+    cmap_key := uint8(pixel_buf[pixel_i])
+    if cmap_key == 0 {
+      last_was_zero = true
+      continue
+    }
+
+    if last_was_zero {
+      /*
+      Transparency is RLE-encoded: a 0 followed by
+      a number indicating how many transparent pixels
+      are there.
+      */
+
+      last_was_zero = false
+      for zero_i := 0; zero_i < int(cmap_key); zero_i++ {
+        fill_i := (pixel_i - 1) + zero_i
+        if fill_i > len(pixels) - 1 {
+          break
+        }
+        pixels[fill_i] = trans_entry
+      }
+      pixel_i += int(cmap_key)
+      continue
+    }
+
+    if pixel_i > len(pixels) - 1 {
+      break
+    }
+
+    pixels[pixel_i] = cmap[cmap_key]
+  }
+  fmt.Println("the pixels", pixels)
+
+  //fmt.Println("processed pixels", pixels)
+
+  px_count := -1
+  for x_idx := 0; x_idx < height; x_idx++ {
+    for y_idx := 0; y_idx < height; y_idx++ {
+      px_count++
+
+      if px_count > len(pixels) - 1 {
+        break
+      }
+
+      if pixels[px_count].transparent {
+        continue
+      }
+
+      rgb := new(color.NRGBA)
+      rgb.R = pixels[px_count].r
+      rgb.G = pixels[px_count].g
+      rgb.B = pixels[px_count].b
+      rgb.A = 0xFF
+
+      fmt.Println("x", x_idx, "y", y_idx, "RGBA", rgb)
+      img.SetNRGBA(x_idx, y_idx, *rgb)
+    }
+  }
+
+  fmt.Println("the image", img.Rect)
+
+  err = png.Encode(out_file, img)
+  if err != nil {
+    fmt.Println("Failed to write sprite png")
+    panic(err)
+  }
+
+  fmt.Println("wrote", out_path)
+}
+
+func ExtractSprites(filename string, cmap []ColorMapEntry) {
+  var err error
+
+  name_bits := strings.Split(filename, ".")
+  out_dir := name_bits[0]
+
+  err = os.MkdirAll(out_dir, os.ModePerm)
+  if err != nil {
+    fmt.Println("Failed to make directory ", out_dir)
+    panic(err)
+  }
+
+  sprite_file, err := os.Open(filename)
+  if err != nil {
+    fmt.Println("Failed to open sprite file", filename)
+    panic(err)
+  }
+  defer sprite_file.Close()
+
+  count_buf := make([]byte, 4)
+  _, err = sprite_file.Read(count_buf)
+  if err != nil {
+    fmt.Println("Failed to read sprite count in", filename)
+    panic(err)
+  }
+
+  file_info, err := sprite_file.Stat()
+  if err != nil {
+    fmt.Println("Failed to stat sprite file: ", err)
+    panic(err)
+  }
+  sprite_file_size := file_info.Size()
+
+  num_sprites := binary.LittleEndian.Uint32(count_buf)
+  fmt.Println(filename, "has", num_sprites, "sprites")
+
+  offset_buf := make([]byte, 4)
+  offsets := make([]SpriteItem, num_sprites)
+
+  // read in the offsets for all sprites in the file
+  // this is the worst code ever ;(
+  for offset_i := 0; offset_i < int(num_sprites); offset_i++ {
+    sprite_file.Read(offset_buf)
+    if err != nil {
+      fmt.Println("Failed to read offset", offset_i, "in", filename)
+      panic(err)
+    }
+
+    this_offset := binary.LittleEndian.Uint32(offset_buf)
+    if this_offset < 1 {
+      // blank entry, can be ignored
+      continue
+    }
+
+    if int64(this_offset) > sprite_file_size {
+      fmt.Println("Offset", this_offset, "is bigger than the file which is ", sprite_file_size)
+      panic(this_offset)
+    }
+
+    offsets[offset_i].offset = this_offset
+  }
+
+  // infer the length of each sprite using the loaded offsets
+  sprite_pt := 0
+  for offset_i := 0; offset_i < int(num_sprites); offset_i++ {
+    // last offset in file, just read until the end
+    if (offset_i + 1) == int(num_sprites) {
+      offsets[offset_i].length = uint32(file_info.Size()) - offsets[offset_i].offset
+      continue
+    }
+
+    if offsets[offset_i + 1].offset == 0 {
+      // find the next non-zero offset
+      sprite_pt = offset_i
+
+      for offsets[sprite_pt].offset == 0 {
+        sprite_pt++;
+        if sprite_pt + 1 == int(num_sprites) {
+          break
+        }
+      }
+
+      if sprite_pt + 1 == int(num_sprites) {
+        // there are only 0 offsets remaining in the file, just
+        // read until the end
+        offsets[offset_i].length = uint32(file_info.Size()) - offsets[offset_i].offset
+
+        continue
+      }
+
+      // use the next non-zero offset we found to calculate the length
+      offsets[offset_i].length = offsets[sprite_pt].offset - offsets[offset_i].offset
+    } else {
+      offsets[offset_i].length = offsets[offset_i + 1].offset - offsets[offset_i].offset
+    }
+  }
+
+  fmt.Println("sprite offsets", offsets)
+
+  //for sprite_i := 0; sprite_i < int(num_sprites); sprite_i++ {
+    //im_filename := fmt.Sprintf("%d.png", sprite_i)
+    //im_path := filepath.Join(out_dir, im_filename)
+
+    //if offsets[sprite_i].offset == 0 {
+       //the file has empty entries or placeholders?
+      //continue
+    //}
+
+    //ExtractSprite(sprite_file, im_path, offsets[sprite_i], cmap)
+  //}
+  im_filename := fmt.Sprintf("%d.png", 0)
+  im_path := filepath.Join(out_dir, im_filename)
+  ExtractSprite(sprite_file, im_path, offsets[2], cmap)
+}
+
 func main() {
   dirs := []string{"GFX", "SFX", "LFX", "DATA"}
 
   for _, sub_dir := range dirs {
-    extractFile(sub_dir)
+    ExtractFile(sub_dir)
   }
+
+  //level_file, err := os.Open("output/DATA/LEV01.DAT")
+  //if err != nil {
+    //fmt.Println("Failed to open level file")
+    //panic(err)
+  //}
+  //defer level_file.Close()
+
+  //sprite_files, err := filepath.Glob("output/DATA/*.SPR")
+  //if err != nil {
+    //fmt.Println("Failed to glob sprite files")
+    //panic(err)
+  //}
+
+  //cmap := GetLevelColorMap(level_file)
+
+  //for _, sprite_filename := range sprite_files {
+    //ExtractSprites(sprite_filename, cmap)
+  //}
 }
--- sprite.rb
+++ sprite.rb
@@ -2,23 +2,19 @@ require 'bundler/setup'
 require 'rmagick'
 require_relative 'defs'
 
-File.open('CONST/GFX/DATA.FIL', 'rb') do |f|
-  archive = Archive::File.read(f)
-  puts "entries in file: #{archive.num}"
-  p archive.i_index
+out_dir = 'output/DATA'
+
+File.open("#{out_dir}/LEV01.DAT", 'rb') do |f|
+  f.seek(12)
+  level_file = Level::File.read(f)
+  level_file.chunks.each do |chunk|
+    File.open("#{out_dir}/LEV01-#{chunk.id}.DAT", 'wb') do |of|
+      of.write(chunk.data.to_binary_s)
+    end
+  end
 end
 
-#File.open('LEV01.DAT', 'rb') do |f|
-  #f.seek(12)
-  #level_file = Level::File.read(f)
-  #level_file.chunks.each do |chunk|
-    #File.open("LEV01-#{chunk.id}.DAT", 'wb') do |of|
-      #of.write(chunk.data.to_binary_s)
-    #end
-  #end
-#end
-
-#File.open('LEV01-CMAP.DAT', 'rb') do |f|
+#File.open("#{out_dir}/LEV01-CMAP.DAT", 'rb') do |f|
   #cmap = Level::Cmap.read(f)
 #end
 
@@ -27,63 +23,73 @@ end
   #p blks.snapshot
 #end
 
-#File.open('HOUSES.SPR', 'rb') do |f|
+Dir["#{out_dir}/*.SPR"].each do |filename|
+  sprite_dir_name = filename.split('/').last.split('.').first
+  sprite_dir = "#{out_dir}/#{sprite_dir_name}"
+  begin
+    Dir.mkdir(sprite_dir)
+  rescue Errno::EEXIST
+  end
 
-  #cmap = Level::Cmap.read(File.read('LEV01-CMAP.DAT'))
+  File.open(filename, 'rb') do |f|
 
-  #imf = ImFile.read(f)
-  #puts "Sprites in file: #{imf.spr_count}"
-  #offsets = imf.offsets.to_a.delete_if{|o| o < 1}.unshift(0)
+    cmap = Level::Cmap.read(File.read("#{out_dir}/LEV01-CMAP.DAT"))
 
-  #offsets.each_with_index do |offset, index|
-    #f.seek(offset)
-    #next_offset = offsets[index + 1]
-    #data_length = next_offset.nil? ? offset : (next_offset - offset)
-    #im = Image.read(f.read(data_length))
+    imf = ImFile.read(f)
+    puts "Sprites in file: #{imf.spr_count}"
+    offsets = imf.offsets.to_a.delete_if{|o| o < 1}.unshift(0)
 
-    #next if im.width < 1 || im.height < 1
+    offsets.each_with_index do |offset, index|
+      f.seek(offset)
+      next_offset = offsets[index + 1]
+      data_length = next_offset.nil? ? offset : (next_offset - offset)
+      im = Image.read(f.read(data_length))
 
-    #puts "Im: #{im.width}, #{im.height}"
+      next if im.width < 1 || im.height < 1
 
-    #last_was_zero = false
-    #color_arr = []
+      puts "Im: #{im.width}, #{im.height}"
 
-    #im.pixels.each do |pixel|
-       #a 0 means the number after it indicates how
-       #many pixels should be transparent as a means
-       #of compressing empty pixels
-      #if pixel == 0
-        #last_was_zero = true
-        #next
-      #end
+      last_was_zero = false
+      color_arr = []
 
-      #if last_was_zero
-        #color_arr = color_arr.concat([nil] * pixel)
-        #last_was_zero = false
-        #next
-      #end
+      im.pixels.each do |pixel|
+        #a 0 means the number after it indicates how
+        #many pixels should be transparent as a means
+        #of compressing empty pixels
+        if pixel == 0
+          last_was_zero = true
+          next
+        end
 
-       #each pixel is a number indicating a position
-       #in the color palette for the level
-      #color_arr.push(cmap.colors[pixel])
-    #end
+        if last_was_zero
+          color_arr = color_arr.concat([nil] * pixel)
+          last_was_zero = false
+          next
+        end
 
-    #img = Magick::Image.new(im.width, im.height)
-    #img.background_color = 'Transparent'
+        #each pixel is a number indicating a position
+        #in the color palette for the level
+        color_arr.push(cmap.colors[pixel])
+      end
 
-    #px_count = -1
+      img = Magick::Image.new(im.width, im.height)
+      img.background_color = 'Transparent'
 
-    #for y_it in 0..im.height-1
-      #for x_it in 0..im.width-1
-        #px_count += 1
-        #next if color_arr[px_count].nil?
+      px_count = -1
 
-        #curr_pix = color_arr[px_count]
-        #rgb_str = "rgb(#{curr_pix["red"]}, #{curr_pix["green"]}, #{curr_pix["blue"]})"
-        #img.pixel_color(x_it, y_it, rgb_str)
-      #end
-    #end
+      for y_it in 0..im.height-1
+        for x_it in 0..im.width-1
+          px_count += 1
+          next if color_arr[px_count].nil?
 
-    #img.write("houses/#{index}.png")
-  #end
-#end
+          curr_pix = color_arr[px_count]
+          rgb_str = "rgb(#{curr_pix["red"]}, #{curr_pix["green"]}, #{curr_pix["blue"]})"
+          img.pixel_color(x_it, y_it, rgb_str)
+        end
+      end
+
+      img.write("#{sprite_dir}/#{index}.png")
+    end
+  end
+end
+
